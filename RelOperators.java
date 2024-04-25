5b158ce209074fc54e4c2a26e056258a36c6179d
Rupkhatri <167824219+Rupkhatri@users.noreply.github.com>
2024-04-25 14:03:17 +0100

    Add files via upload

---
+++ RelOperators.java
@@ -0,0 +1,16 @@
+package relational_operators;
+
+public class RelOperators {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+
+
+    }
+}
