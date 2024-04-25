bd38f0c06f0ccdf16f719406b5573d46c001b5c7
Rupkhatri <167824219+Rupkhatri@users.noreply.github.com>
2024-04-25 14:00:38 +0100

    Add files via upload
    Byte example ...homework

---
+++ ByteExample.java
@@ -0,0 +1,35 @@
+package data_type;
+
+public class ByteExample {
+    public static void main(String[] args) {
+        int n, a;
+        n=127;
+        a=177;
+        System.out.println(n);
+        System.out.println(a);
+
+        int num= 5464564;
+        System.out.println(num);
+
+        long numb = 15000000000L;
+        System.out.println(numb);
+
+        double number = 79.679d;
+        System.out.println(number);
+
+        float numberr = 67;
+        System.out.println(numberr);
+
+        char alpha = 'J';
+        char d = 65, b = 66, c = 67;
+        System.out.println(alpha);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+
+
+
+    }
+}
