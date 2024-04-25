be0e534b487a45142c416fda04810f542a3cdab0
Rupkhatri <167824219+Rupkhatri@users.noreply.github.com>
2024-04-25 14:03:42 +0100

    Add files via upload

---
+++ UnOperator.java
@@ -0,0 +1,14 @@
+package unary_operator;
+
+public class UnOperator {
+    public static void main(String[] args) {
+        int a = 20;
+        boolean b = true;
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
+        System.out.println(!b);
+
+    }
+}
