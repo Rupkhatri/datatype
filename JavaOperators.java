6db00940bfd4c6657aec1ba67a343b0095f923b9
Rupkhatri <167824219+Rupkhatri@users.noreply.github.com>
2024-04-25 14:02:49 +0100

    Add files via upload

---
+++ JavaOperators.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class JavaOperators {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 25;
+        int c;
+        System.out.println(c = a);
+        System.out.println(b += a);
+        System.out.println(b -= a);
+        System.out.println(b *= a);
+        System.out.println(b /= a);
+        System.out.println(b %= a);
+        System.out.println(b ^= a);
+
+    }
+}
